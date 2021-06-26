b961d21e4c1d5e735b8ea64239e4477ccafad8ae
Thomas LINTANF <thomas.lintanf@free.fr>
2021-06-26 21:27:16 +0200

    base structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InfoHeaderComponent } from './info-header/info-header.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoHeaderComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
