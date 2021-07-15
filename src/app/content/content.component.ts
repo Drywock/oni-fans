d34c8d46e86313deb3953ae26ac42ac58b2069f6
Thomas LINTANF <thomas.lintanf@efrei.net>
2021-07-15 14:59:19 +0200

    Refactor post to be in a service

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import POSTS from 'src/assets/posts.json';
+import { PostsService } from '../services/posts.service';
 
 @Component({
   selector: 'app-content',
@@ -7,12 +9,21 @@ import POSTS from 'src/assets/posts.json';
   styleUrls: ['./content.component.scss']
 })
 export class ContentComponent implements OnInit {
+  private sub = new Subscription;
 
-  constructor() { }
+  constructor(
+    private postsService: PostsService,
+  ) { }
 
-  post = POSTS;
+  posts = POSTS;
 
   ngOnInit(): void {
+    this.sub.add(this.postsService.getPosts().subscribe({
+      next: (posts) => {
+        this.posts = posts;
+      }
+    })
+    )
   }
 
 }
---
+++ src/app/services/posts.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import POSTS from 'src/assets/posts.json';
+
+export interface IAccount {
+  name: string;
+  handle: string;
+}
+
+export interface IContent {
+  text: string;
+  img: string;
+  isLewd: boolean;
+}
+
+export interface IAuthor {
+  name: string;
+  link: string;
+}
+
+export interface IPost {
+    author: IAccount; 
+    date: string,
+    content: IContent;
+    credit: IAuthor;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+  private _posts: Observable<IPost>;
+
+  constructor() { 
+    this._posts = of(POSTS);
+  }
+
+  getPosts() {
+    return this._posts;
+  }
+}
